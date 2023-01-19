ef1556d605176e4db924274c013c06c33c715093
Austin Lucas <aclucas196@gmail.com>
2023-01-19 14:31:26 -0500

    EoL 143

--- 05 Doc Obj Model/index.js
+++ 05 Doc Obj Model/index.js
@@ -1,1 +1,20 @@
-document.querySelector('h1').innerHTML = "goodbye!"
+// Get Header
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+
+// Update Header Var
+heading.innerHTML = 'Updated by JS';
+heading.style.color = 'blue'
+
+// Click on the check box
+document.querySelector('input').click();
+
+function flipH1() {
+    if (heading.style.color == 'red'){
+        heading.style.color = 'blue'
+    }
+    else {
+        heading.style.color = 'red'
+    }
+}
+
+document.querySelector(".list a").style.color = "green"
